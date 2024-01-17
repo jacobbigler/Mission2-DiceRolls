0e7cc2af8b88936426e81bf8857f2ab420c64390
jacobbigler <jacob.bigler03@gmail.com>
2024-01-17 15:08:34 -0700

    Add project files.

---
+++ DiceRoll.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2_DiceRolls
+{
+    internal class DiceRoll //dope class for rolling two dice
+    {
+        private Random random;//call random class to generate random numbers
+
+        //constructor to initialize random num generator
+        public DiceRoll() { 
+            random = new Random();
+        }
+
+        //Simulate dice rolls for the requested number of rolls
+        public int[] SimulateRolls(int numRolls)
+        {
+            //array to store the count of each dice combination
+            int[] results = new int[13];//Because 0 is not used, 2 represents 2, 3 represents 3, and so on
+
+            //loop through specified number of rolls
+            for (int i = 0; i < numRolls; i++)
+            {
+                //Two dice are assignemd random values between 1 and 6
+                int dice1 = random.Next(1, 7);
+                int dice2 = random.Next(1, 7);
+
+                //The results are added up
+                int sum = dice1 + dice2;
+
+                //Results stored in an array
+                results[sum]++;
+            }
+
+            return results;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿
+using Mission2_DiceRolls;
+using System.ComponentModel.Design;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        //Create variable for asking for the num of rolls
+        int num = 0;
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");//Welcome :)
+
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");//Ask for number of rolls
+
+        while (true)
+        {
+            int userInput = int.Parse(System.Console.ReadLine());
+
+            if (userInput > 0 && userInput <= 2147483647)
+            {
+                num = userInput; // Store the number of rolls input into the num variable
+                break; // Exit the loop if the input is greater than 0
+            }
+            else
+            {
+                System.Console.WriteLine("Please enter a number greater than 0 and less than 2,147,483,647");
+            }
+        }
+
+        //Bring in the other class
+        DiceRoll dr = new DiceRoll();
+
+        //Call method to simulate the rollsintint
+        int[] results = dr.SimulateRolls(num);
+
+        //Call Print method and pass parameters
+        PrintHistogram(results, num);
+
+    }
+
+    private static void PrintHistogram(int[] results, int num)
+    {
+        //print header
+        System.Console.WriteLine("\nHistogram");
+
+        //go through possible dice combinations
+        for (int i = 2; i <= 12; i++)
+        {
+            //calculate percentage of times the combination was rolled
+            int percentage = results[i] * 100 / num;
+
+            //print combination and its corresponding percentage using "*"
+            System.Console.Write($"{i,2}: ");
+
+            for (int j = 0; j < percentage; j++)
+            {
+                System.Console.Write("*");
+            }
+
+            //Add the percentage for better readability
+            System.Console.WriteLine($" ({percentage}%)");
+        }
+    }
+}
